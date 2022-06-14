1a40c3b68bb23aae362652080ff8d03c20a88423
裕 <46030302+uu890817@users.noreply.github.com>
2022-06-14 21:39:13 +0800

    新增:開啟周圍為0的區塊

--- cell.py
+++ cell.py
@@ -3,7 +3,7 @@ import random
 import settings
 import ctypes
 import sys
-import time ##
+
 
 class Cell:
     all = []
@@ -14,6 +14,7 @@ class Cell:
         self.is_opened = False
         self.is_mine_candidate = False
         self.cell_btn_object = None
+        self.is_zero = False #(新增)周圍炸彈數量是否為0
         self.x = x
         self.y = y
 
@@ -40,35 +41,43 @@ class Cell:
             font=("", 30)
         )
         Cell.cell_count_label_object = lbl
-#----------------新增CODE 1----------------#
+#----------------第一次不要按到地雷----------------#
     @staticmethod
-    def reset():
+    def reset_mines():
         for pick_reset in Cell.all:
             pick_reset.is_mine = False
-
-#----------------新增CODE 1----------------#
     def left_click_actions(self, event):
-        #----------------新增CODE 2----------------#
-        print("點選位置參數:","(",self.x,",",self.y,")",self.is_mine,self.cell_count)     #DeBug用
-        if self.is_mine == True and self.cell_count == 36:     #判斷第一次點即是否為地雷
-            print("----觸發地雷重置----")                       #DeBug用
-            self.reset()                                       #歸零地雷
+
+        print("\n-------點選位置:","(",self.x,",",self.y,")","是否為炸彈:",self.is_mine,"剩餘格數:",self.cell_count,"-------")     #DeBug用
+        if self.is_mine == True and self.cell_count == settings.CELL_COUNT:     #判斷第一次點即是否為地雷
+            print("------------觸發地雷重置------------")                        #DeBug用
+            self.reset_mines()                                 #清除地雷
             Cell.randomize_mines()                             #重新排放地雷
-            self.left_click_actions(event)                     #模擬滑鼠左鍵點選
-            return                                             #離開點選事件
-        print(event)  #DeBug用
-        #----------------新增CODE 2----------------#
-        
+            self.left_click_actions(event)                     
+            return                                             
+        #print(event)  #DeBug用
+#----------------第一次不要按到地雷 END----------------#
+        #for cell_zero in self.all:
+        #    cell_zero.is_zero = False
+
         if self.is_mine:
             self.show_mine()
         else:
-            if self.surrounded_cells_mines_length == 0:
-                for cell_obj in self.surrounded_cells:
-                    cell_obj.show_cell()
-            self.show_cell()
+            if self.count_mines(self) == 0:
+                self.around_show_cell(self)
+                self.is_zero = True
+                for cell_obj in self.get_surrounded_cells(self.x,self.y):
+                    self.around_show_cell(cell_obj)
+                    if self.count_mines(cell_obj) == 0:
+                        cell_obj.is_zero = True
+                        self.get_around_and_show_cell(cell_obj)
+            self.around_show_cell(self)
+            self.cell_btn_object.configure(bg='#66CCFF')#SystemButtonFace #標記按下位置
+            
             # If Mines count is equal to the cells left count, player won
             if Cell.cell_count == settings.MINES_COUNT:
                 ctypes.windll.user32.MessageBoxW(0, 'Congratulations! You won the game!', 'Game Over', 0)
+                exit()
 
         # Cancel Left and Right click events if cell is already opened:
         self.cell_btn_object.unbind('<Button-1>')
@@ -79,52 +88,67 @@ class Cell:
         for cell in Cell.all:
             if cell.x == x and cell.y == y:
                 return cell
-
-    @property
-    def surrounded_cells(self):
+##-------------------------新增或修改部分-------------------------##
+    def get_surrounded_cells(self,input_x,input_y): #取得周圍8格CELL #原本的surrounded_cells()
         cells = [
-            self.get_cell_by_axis(self.x - 1, self.y -1),
-            self.get_cell_by_axis(self.x - 1, self.y),
-            self.get_cell_by_axis(self.x - 1, self.y + 1),
-            self.get_cell_by_axis(self.x, self.y - 1),
-            self.get_cell_by_axis(self.x + 1, self.y - 1),
-            self.get_cell_by_axis(self.x + 1, self.y),
-            self.get_cell_by_axis(self.x + 1, self.y + 1),
-            self.get_cell_by_axis(self.x, self.y + 1)
+            self.get_cell_by_axis(input_x - 1, input_y -1 ), #左上
+            self.get_cell_by_axis(input_x - 1, input_y    ), #左
+            self.get_cell_by_axis(input_x - 1, input_y + 1), #左下
+            self.get_cell_by_axis(input_x,     input_y - 1), #正上
+            self.get_cell_by_axis(input_x + 1, input_y - 1), #右上
+            self.get_cell_by_axis(input_x + 1, input_y    ), #右
+            self.get_cell_by_axis(input_x + 1, input_y + 1), #右下
+            self.get_cell_by_axis(input_x,     input_y + 1)  #正下
         ]
-
         cells = [cell for cell in cells if cell is not None]
         return cells
 
-    @property
-    def surrounded_cells_mines_length(self):
+    def count_mines(self,input_cell): #計算周圍炸彈數量  #原本的surrounded_cells_mines_length()
+        
         counter = 0
-        for cell in self.surrounded_cells:
+        for cell in self.get_surrounded_cells(input_cell.x,input_cell.y):
             if cell.is_mine:
                 counter += 1
-
         return counter
 
-    def show_cell(self):
-        if not self.is_opened:
-            Cell.cell_count -= 1
-            self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
-            # Replace the text of cell count label with the newer count
-            if Cell.cell_count_label_object:
-                Cell.cell_count_label_object.configure(
-                    text=f"Cells Left:{Cell.cell_count}"
-                )
-            # If this was a mine candidate, then for safety, we should
-            # configure the background color to SystemButtonFace
-            self.cell_btn_object.configure(
-                bg='SystemButtonFace'
-            )
+    def get_around_and_show_cell(self,input_cell): #取得周圍並開啟為0的部分
+        #print("\nget_around_and_show_cell ->","input_cell:",input_cell)
+        cells = self.get_surrounded_cells(input_cell.x,input_cell.y)
+        counter = self.count_mines(input_cell)
+        if counter == 0 :
+            for cell in cells:
+                #解除cell的按鍵綁定
+                cell.cell_btn_object.unbind('<Button-1>')
+                cell.cell_btn_object.unbind('<Button-3>')
+                if self.count_mines(cell) == 0 and cell.is_zero == False :
+                    cell.is_zero = True
+                    self.get_around_and_show_cell(cell)#遞迴
+                self.around_show_cell(cell)    
+        self.around_show_cell(input_cell)
+    
+        #print("get_around_and_show_cell ->",input_cell ,"counter:",counter)
+        return
 
-        # Mark the cell as opened (Use is as the last line of this method)
-        self.is_opened = True
+    def around_show_cell(self,input_cell): #原本的show_cell()
+        if not input_cell.is_opened:
+            #print("around_show_cell() -> 本次開啟:",input_cell,"周圍炸彈數量",self.count_mines(input_cell))
+            Cell.cell_count -= 1
+            input_cell.cell_btn_object.configure(text=self.count_mines(input_cell))
+            if Cell.cell_count_label_object:
+                Cell.cell_count_label_object.configure(text=f"Cells Left:{Cell.cell_count}")
+
+            input_cell.cell_btn_object.configure(
+                 bg='gray'#SystemButtonFace
+                 )
+            
+        input_cell.is_opened = True
+        
+
+##------------------------新增或修改部分END------------------------##
 
     def show_mine(self):
         self.cell_btn_object.configure(bg='red')
+        print("\n************爆炸************\n")
         ctypes.windll.user32.MessageBoxW(0, 'You clicked on a mine', 'Game Over', 0)
         sys.exit()
 
@@ -151,7 +175,7 @@ class Cell:
         for picked_cell in picked_cells:
             picked_cell.is_mine = True
             
-        print("地雷位置:\n",picked_cells)#DeBug用
+        print("\n地雷位置:\n",picked_cells)#DeBug用
         
     def __repr__(self):
         return f"Cell({self.x}, {self.y})"
